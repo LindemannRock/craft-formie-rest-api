a35e98c6f05132768b946ed0b2282de218111cbd
bhlindemann <bh@lindemannrock.com>
2026-04-20 15:49:04 +0400

    docs(config): add template for Formie REST API configuration settings
    This commit introduces a new config.php file as a template for the Formie REST API settings. It provides guidance on how to override default settings by copying it to 'craft/config' as 'formie-rest-api.php'.

---
+++ src/config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Formie REST API config.php
+ *
+ * This file exists only as a template for the Formie REST API settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'formie-rest-api.php'
+ * and make your changes there to override default settings.
+ *
+ * @since 3.3.1
+ */
+
+return [
+    // Global settings
+    '*' => [
+        // ========================================
+        // GENERAL SETTINGS
+        // ========================================
+
+        /**
+         * Plugin name shown in the Control Panel.
+         */
+        // 'pluginName' => 'Formie REST API',
+    ],
+];
